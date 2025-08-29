00bd40ec78fab3c7e017104533d02f8ab1440cf1
PabloLabs <pablodesouzadev@gmail.com>
2025-08-29 21:08:27 +0000

    Pastas prearadas para o exercicio em C#

---
+++ Dia002/Program.cs
@@ -0,0 +1,16 @@
+﻿﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite seu nome:");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Digite sua idade:");
+        string idadeTexto = Console.ReadLine();
+        int idade = int.Parse(idadeTexto);  // Converte texto para número inteiro
+
+        Console.WriteLine("Seu nome é " + nome + " e você tem " + idade + " anos.");
+    }
+}
