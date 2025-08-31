c667d75a76d86afb50452810313378d4dbb7dfef
PabloLabs <pablodesouzadev@gmail.com>
2025-08-31 20:17:03 +0000

    Implementa programa que lê dois números inteiros, calcula e exibe o resto da divisão

--- Dia006/Program.cs
+++ Dia006/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    Console.Write("Digite o primeiro número (dividendo):");
+    int numero1 = int.Parse(Console.ReadLine());
+
+    Console.Write("Digite o segundo número (divisor):");
+    int numero2 = int.Parse(Console.ReadLine());
+
+    int resto = numero1 % numero2;
+
+    Console.WriteLine($"O resto da divisão de {numero1} por {numero2} é: {resto}");
+
+
+}
+}
