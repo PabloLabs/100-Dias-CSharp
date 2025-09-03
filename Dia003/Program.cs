9d80daac58a6871588f4437cb6a708ab0eb4eb77
PabloLabs <pablodesouzadev@gmail.com>
2025-09-03 22:20:10 +0000

    Dia 3: Somar dois números simples

--- Dia003/Program.cs
+++ Dia003/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o primeiro número: ");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        int soma = numero1 + numero2;
+
+        Console.WriteLine($"A soma dos números é: {soma}");
+    }
+}
