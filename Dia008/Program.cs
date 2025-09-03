168157dbb3690f2e3dea1957be537d58ca9b4766
PabloLabs <pablodesouzadev@gmail.com>
2025-09-03 21:53:50 +0000

    Dia 8: Calcular área de um quadrado

--- Dia008/Program.cs
+++ Dia008/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o valor do lado do quadrado:");
+        double lado = double.Parse(Console.ReadLine());
+
+        double area = lado * lado;
+
+        Console.WriteLine($"A área do quadrado é: {area:F2}");
+    }
+}
