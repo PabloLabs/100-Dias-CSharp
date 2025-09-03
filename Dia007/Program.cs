10da4454450cd994e67356c12beb343b98eee06c
PabloLabs <pablodesouzadev@gmail.com>
2025-09-03 21:23:51 +0000

    Dia 7: Verificar se um número é par ou ímpar

--- Dia007/Program.cs
+++ Dia007/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Formats.Asn1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um numero inteiro: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("numero é par.");
+
+        }
+        else {
+            Console.Write("O numero é impar.");
+        }
+    }
+}
