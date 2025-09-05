d935f49250cffdf900d17594ea3669ca4c445e47
PabloLabs <pablodesouzadev@gmail.com>
2025-09-05 21:57:01 +0000

    Dia 9: Mostrar a tabuada de um número de 1 a 10

--- Dia009/Program.cs
+++ Dia009/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+
+{
+    static void Main(String[] args)
+{
+    Console.Write("Digite um numero inteiro para ver a tabuada: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Tabuada do {numero}:");
+
+    for (int i = 1; i <= 10; i++)
+    {
+        int resultado = numero * i;
+        Console.WriteLine($"{numero} x {i} = {resultado}");
+    }
+
+}
+
+    }
