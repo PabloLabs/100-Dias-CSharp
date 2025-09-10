30b54183d6a5c6c3d6318915afb0e25d40ca5a7f
PabloLabs <pablodesouzadev@gmail.com>
2025-09-10 14:41:57 +0000

    feat: converte minutos em horas e minutos com divisão inteira e resto (%)

--- Dia010/Program.cs
+++ Dia010/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+  class Program
+
+{
+    static void Main(string[] args)
+
+    {
+
+        Console.Write("Digite o total de minutos: ");
+
+    int minutosTotais = int.Parse(Console.ReadLine());
+
+        int horas = minutosTotais / 60;
+
+        int minutos = minutosTotais % 60;
+
+        Console.WriteLine($"{minutosTotais} minutos equivalem a {horas} hora(s).");
+    }
+}
+
+
