18b4772ea7fe76acadb9457b24f0bd7963a8b719
PabloLabs <pablodesouzadev@gmail.com>
2025-09-19 00:30:44 +0000

    feat: Exercício Dia 011 - Implementação do loop do/while com confirmação do usuário para continuar ou sair

--- Dia011/Program.cs
+++ Dia011/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string resposta;
+        do
+        {
+            Console.Write("Deseja Continuar? (S/N): ");
+            resposta = Console.ReadLine().ToUpper();
+
+            if (resposta != "S" && resposta != "N")
+            {
+                Console.WriteLine("Digite S para sim ou N para não.");
+            }
+        }
+        while (resposta != "N");
+
+        Console.WriteLine("Encerrando programa...");
+    }
+}
