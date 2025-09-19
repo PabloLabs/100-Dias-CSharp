2d0b4997b165985d823dd458a9fbe58d2cfec057
PabloLabs <pablodesouzadev@gmail.com>
2025-09-19 23:41:15 +0000

    Atualização do exercício 12

--- Dia012/Program.cs
+++ Dia012/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int tamanho = 5;
+        double[] notas = new double[tamanho];
+        double soma = 0;
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            Console.WriteLine("Digite a nota: ");
+            notas[i] = double.Parse(Console.ReadLine());
+            soma += notas[i];
+        }
+
+        double media = soma / tamanho;
+        Console.WriteLine("Média: " + media);
+    }
+}
